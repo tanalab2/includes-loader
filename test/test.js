0e37f8510f1ab0db5c2346a373561dce19627fb4
kenji tanaka <tanalab2@gmail.com>
2019-04-15 09:52:32 +0900

    test: add pattern test

--- test/test.js
+++ test/test.js
@@ -26,6 +26,26 @@ describe('by runner', () => {
         });
     });
 
+      it('pattern', function() {
+        runLoaders({
+            resource: path.resolve(__dirname, './files/file_pattern.conf'),
+            loaders: [
+                {
+                  loader: loaderPath,
+                  options: {
+                    pattern: {
+                      re: /<!--#\s*include\s+("|')(.+)\1\s*?-->/,
+                      index: 2
+                    }
+                  }
+                }
+            ],
+            readResource: fs.readFile.bind(fs)
+        }, (err, result) => {
+            assert.equal('foo\nbar\nfoo\nbar\nconf', result['result'][0]);
+        });
+    });
+
     it('md', function() {
         runLoaders({
             resource: path.resolve(__dirname, './files/file.md'),
